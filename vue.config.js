87c8a6e37355f709e1365ac314c33dd8ba3309e5
scred <scred666@gmail.com>
2021-03-06 20:38:17 +0200

    Update README.md

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/demos/3d-shapes',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/demos/memory-game',
   css: {
     loaderOptions: {
       sass: {
